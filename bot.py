ec15c232f7b827feae399649c8b7c743e4abdbce
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-10-14 14:32:42 -0300

    add bot

--- bot.py
+++ bot.py
@@ -1,7 +1,52 @@
 import os
 
 from dotenv import load_dotenv
-from tg_api import SendMessageRequest, SyncTgClient
+from tg_api import (
+    SyncTgClient,
+    SendMessageRequest,
+    SendBytesPhotoRequest,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+
+
+def send_message(token, tg_chat_id):
+    #  Отправить себе текстовое сообщение от имени tg-бота
+    with SyncTgClient.setup(token):
+        tg_request = SendMessageRequest(
+            chat_id=tg_chat_id, text="Message proofs high level usage."
+        )
+        tg_request.send()
+
+
+def send_message_with_keyboard(token, tg_chat_id):
+    # Отправить себе текстовое сообщение с кнопками от имени tg-бота
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="button_1", callback_data="test"),
+                InlineKeyboardButton(text="button_2", callback_data="test"),
+            ],
+        ],
+    )
+    with SyncTgClient.setup(token):
+        tg_request = SendMessageRequest(
+            chat_id=tg_chat_id,
+            text="Message proofs keyboard support.",
+            reply_markup=keyboard,
+        )
+        tg_request.send()
+
+
+def send_photo(token, tg_chat_id, photo_filename):
+    #  Отправить себе сообщение с картинкой от имени tg-бота
+    with SyncTgClient.setup(token):
+        with open(photo_filename, "rb") as f:
+            photo_content = f.read()
+        tg_request = SendBytesPhotoRequest(
+            chat_id=tg_chat_id, photo=photo_content, filename=photo_filename
+        )
+        tg_request.send()
 
 
 def main():
@@ -9,11 +54,11 @@ def main():
     token = os.environ["TG_BOT_TOKEN"]
     tg_chat_id = os.environ["TG_CHAT_ID"]
 
-    with SyncTgClient.setup(token):
-        tg_request = SendMessageRequest(
-            chat_id=tg_chat_id, text="Message proofs high level usage."
-        )
-        tg_request.send()
+    send_message(token, tg_chat_id)
+
+    send_message_with_keyboard(token, tg_chat_id)
+
+    send_photo(token, tg_chat_id, "image.jpeg")
 
 
 if __name__ == "__main__":
